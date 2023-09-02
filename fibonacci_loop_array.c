9244b127c089b051d751be626aae5a81d585768a
Mayankdev0923 <143903748+Mayankdev0923@users.noreply.github.com>
2023-09-03 00:40:15 +0530

    Add files via upload

---
+++ fibonacci_loop_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+  int main() {
+    int n = 10;
+    int fib[10];
+    fib[0]=0;
+    fib[1]=1;
+    
+    for (int i=2 ; i<n ;i++) {
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+    
+    for (int i=0 ; i<n ; i++) {
+      printf("%d ,\t",fib[i]);
+    }
+    return 0;
+  }
